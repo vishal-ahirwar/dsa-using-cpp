8e46fe5f68182c2b33fe537b955988884a6aabf4
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2025-07-23 22:17:39 +0530

    modified:   .clangd 	modified:   sorting-techniques/src/comparison_base_sorting/bubble.hxx

--- sorting-techniques/src/comparison_base_sorting/bubble.hxx
+++ sorting-techniques/src/comparison_base_sorting/bubble.hxx
@@ -2,16 +2,16 @@
 #define BUBBLE_HXX
 #include <cassert>
 #include <cstddef>
-#include <iostream>
 #include <utility>
 template <typename T>
 void bubbleSort(T& data, size_t size, size_t number_of_sorts = 0)
 {
     if (!number_of_sorts)
         number_of_sorts = size;
+
     for (size_t i = 0; i < size - 1 && number_of_sorts > 0; ++i, --number_of_sorts)
     {
-        bool is_sorted{true};
+        bool is_sorted = true;
         for (size_t j = 0; j < size - i - 1; ++j)
         {
             if (data[j] > data[j + 1])
